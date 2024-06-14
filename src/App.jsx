c66e5c25b987d3bf78ef69a76b0b6a558a79d4ea
Mandeep Singh <mandeepsingh.71ms31@gmail.com>
2024-06-14 12:33:24 -0400

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './Components/Home/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Home/>
     </div>
   )
 }
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Home.css";
+import profile_img from "../../assets/profile_img.svg";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img src={profile_img} alt="" />
+      <h1>
+        <span>I'm Mandeep Singh,</span> <br></br> Full-Stack Developer based in
+        Canada.
+      </h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis,
+        veritatis tempore numquam voluptatem nulla laudantium aspernatur porro
+        fugit a sed!
+      </p>
+      <div className="home-action">
+        <div className="home-connect">Connect with me</div>
+        <div className="home-resume">My resume</div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
