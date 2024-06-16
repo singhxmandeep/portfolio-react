b997300705415bbcc0f0a317a74bc728f6aeba74
Mandeep Singh <mandeepsingh.71ms31@gmail.com>
2024-06-16 01:27:43 -0400

    add functionality

---
+++ src/Components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Footer.css";
+import footer_logo from "../../assets/footer_logo.svg";
+import user_icon from "../../assets/user_icon.svg";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footer-top">
+        <div className="footer-top-left">
+          <img src={footer_logo} alt="" />
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Saepe enim
+            unde, consequuntur facilis cum adipisci magni beatae laudantium
+            voluptatibus maxime provident.
+          </p>
+        </div>
+        <div className="footer-top-right">
+          <div className="footer-email-input">
+            <img src={user_icon} alt="" />
+            <input type="email" name="email" placeholder="Enter your email" />
+          </div>
+          <div className="footer-subscribe">Subscribe</div>
+        </div>
+      </div>
+        <hr />
+        <div className="footer-bottom">
+          <p className="footer-bottom-left">
+            Lorem ipsum dolor, sit amet consectetur adipisicing.
+          </p>
+          <div className="footer-bottom-right">
+            <p>Term of Services</p>
+            <p>Privacy Policy</p>
+            <p>Connect with me</p>
+          </div>
+        </div>
+    </div>
+  );
+};
+
+export default Footer;
